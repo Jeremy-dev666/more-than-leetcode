1674a0178a33d8508754a9df8be684bba5503b5e
People <chen.zhaom@northeastern.edu>
2026-06-08 16:19:36 +0800

    Time: 2 ms (61.52%), Space: 19.5 MB (19.58%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -3,28 +3,22 @@ from collections import Counter
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        # 频次超过一半一定无解
-        # 每次贪心的先放频次最高的
-        # 取出来用了的，下一步不能用（延迟放置）
-
-        freq = Counter(s)
         n = len(s)
-        for cnt in freq.values():
-            if cnt > (n + 1) // 2:
+        freq = Counter(s)
+        for f in freq.values():
+            if f > (n + 1) // 2:
                 return ""
-        
-        max_heap = [(-cnt, char) for char, cnt in freq.items()]
-        heapq.heapify(max_heap)
+
+        mx_heap = [(-cnt, char) for char, cnt in freq.items()]
+        heapq.heapify(mx_heap)
 
         ans = []
         prev_cnt, prev_char = 0, ""
-        while max_heap:
-            cnt, char = heapq.heappop(max_heap)
+        while mx_heap:
+            cnt, char = heapq.heappop(mx_heap)
             ans.append(char)
             if prev_cnt < 0:
-                heapq.heappush(max_heap, (prev_cnt, prev_char))
-            
-            prev_cnt, prev_char = cnt+1, char
-        
+                heapq.heappush(mx_heap, (prev_cnt, prev_char))
+            prev_cnt, prev_char = cnt + 1, char
+
         return "".join(ans)
-        
