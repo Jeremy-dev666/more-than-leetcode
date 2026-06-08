11440ccfaf75f5409969436df169b95026bd2b9f
People <chen.zhaom@northeastern.edu>
2026-06-08 15:33:20 +0800

    Time: 0 ms (100%), Space: 19.7 MB (15.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,32 +3,26 @@ class Solution:
         m, n = len(nums1), len(nums2)
         if m > n:
             return self.findMedianSortedArrays(nums2, nums1)
-        
-        left, right = 0, m
-        while left <= right:
-            # 定义两个数组的分割点
-            i = left + (right - left) // 2
-            j = (m + n + 1) // 2 - i
 
-            # 通过分割点确认分割左右边界
-            nums1_left_end = -math.inf if i == 0 else nums1[i - 1]
-            nums1_right_start = math.inf if i == m else nums1[i]
-            nums2_left_end = -math.inf if j == 0 else nums2[j - 1]
-            nums2_right_start = math.inf if j == n else nums2[j]
+        # 这个边界非常重要，L可以落在整个数组的有边界外，表示右侧段个数为零
+        l, r = 0, m
+        while l <= r:
+            div1 = l + (r - l) // 2
+            div2 = (m + n + 1) // 2 - div1
+            
+            div1_left = -float('inf') if div1 == 0 else nums1[div1 - 1]
+            div1_right = float('inf') if div1 == m else nums1[div1]
+            div2_left = -float('inf') if div2 == 0 else nums2[div2 - 1]
+            div2_right = float('inf') if div2 == n else nums2[div2]
 
-            if nums1_left_end <= nums2_right_start \
-                    and nums2_left_end <= nums1_right_start:
-                left_max = max(nums1_left_end, nums2_left_end)
-                right_min = min(nums1_right_start, nums2_right_start)
-
+            if div1_left <= div2_right and div1_right >= div2_left:
                 if (m + n) % 2 == 1:
-                    return left_max
+                    return max(div1_left, div2_left)
                 else:
-                    return (left_max + right_min) / 2.0
-
-            if nums1_left_end > nums2_right_start:
-                right = i - 1
+                    return (max(div1_left, div2_left) + min(div1_right, div2_right)) / 2.0
+            elif div1_left > div2_right:
+                r = div1 - 1
             else:
-                left = i + 1
-
+                l = div1 + 1
+                
         return -1.0
