65710223aec056f61097960d24ad0567177dc0fa
People <chen.zhaom@northeastern.edu>
2026-06-08 15:12:27 +0800

    Time: 0 ms (100%), Space: 19.4 MB (39.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -2,24 +2,21 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
         left, right = 0, n
-
         while left < right:
             mid = left + (right - left) // 2
+            # 1.命中
             if nums[mid] == target:
                 return mid
-
-            # 查mid落在哪段数组里
-            if nums[mid] >= nums[left]:
-                # 确认target是落在mid所指的哪个区间数组内
+            # 2.mid左侧有序
+            elif nums[mid] >= nums[left]:
                 if target >= nums[left] and target < nums[mid]:
                     right = mid
                 else:
                     left = mid + 1
+            # 3.mid右侧有序
             else:
                 if target <= nums[right - 1] and target > nums[mid]:
                     left = mid + 1
                 else:
                     right = mid
-
         return -1
-
