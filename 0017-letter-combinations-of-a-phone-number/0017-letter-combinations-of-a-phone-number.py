0ee56c187edb65766a2b6a6e2af6e03b33ad7f96
People <chen.zhaom@northeastern.edu>
2026-06-14 23:17:53 +0800

    Time: 0 ms (100%), Space: 19.3 MB (77.88%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,10 +1,6 @@
-from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return []
-        self.digits = digits
-        self.letters = {
+        dic = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -14,18 +10,18 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
+        def backtrack(path, index):
+            if len(path) == len(digits):
+                ans.append("".join(path))
+                return
+            
+            number = digits[index]
+            letters = dic[number]
+            for l in letters:
+                path.append(l)
+                backtrack(path, index + 1)
+                path.pop()
 
-        self.ans = []
-        self.backtrack(0, [])
-        return self.ans
-
-    def backtrack(self, index: int, path: List[str]) -> None:
-        if len(path) == len(self.digits):
-            self.ans.append("".join(path))
-            return
-
-        possible_letters = self.letters[self.digits[index]]
-        for letter in possible_letters:
-            path.append(letter)
-            self.backtrack(index + 1, path)
-            path.pop()
+        ans = []
+        backtrack([], 0)
+        return ans
