35e7729101abb96fbb93180f72dab6bde13807bc
People <chen.zhaom@northeastern.edu>
2026-06-16 22:47:16 +0800

    Time: 134 ms (9.39%), Space: 32.1 MB (95.11%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.py
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.py
@@ -1,23 +1,7 @@
 class Solution:
     def minOperations(self, nums: list[int]) -> int:
-        n = len(nums)
-        if n <= 1:
-            return 0
-
-        prev_val = nums[0]   # a[i-1]
-        prev_need = 0        # need[i-1]
         ans = 0
-
-        for i in range(1, n):
-            if nums[i] < prev_val:
-                need = prev_val - nums[i]  # a[i] = prev_val
-            else:
-                prev_val = nums[i]         # a[i] = nums[i]
-                need = 0
-
-            if need > prev_need:
-                ans += need - prev_need
-
-            prev_need = need
-
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                ans += nums[i - 1] - nums[i]
         return ans
