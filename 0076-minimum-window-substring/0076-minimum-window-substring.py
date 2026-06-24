fb4a4ce20702d74affb370e0d15c69f88853499c
People <chen.zhaom@northeastern.edu>
2026-06-24 12:39:28 +0800

    Time: 93 ms (39.78%), Space: 19.6 MB (73.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,38 +1,29 @@
-
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         m, n = len(s), len(t)
-        freq = [0] * 128
-        types = 0
-
-        for i in range(n):
-            idx = ord(t[i])
-            if freq[idx] == 0:
-                types += 1
-            freq[idx] += 1
-
-        
-        start = left = right = 0
-        min_len = float('inf')
-        w = [0] * 128
+        if n > m:
+            return ""
+        ft = Counter(t)
+        fs = Counter()
+        l = r = 0
         valid = 0
-        while right < m:
-            idx = ord(s[right])
-            w[idx] += 1
-            if freq[idx] != 0 and w[idx] == freq[idx]:
-                valid += 1
-
-            while valid == types:
-                cur_len = right - left + 1
-                if cur_len < min_len:
-                    min_len = cur_len
-                    start = left
-
-                l_idx = ord(s[left])
-                w[l_idx] -= 1
-                if freq[l_idx] != 0 and freq[l_idx] > w[l_idx]:
-                    valid -= 1
-                left += 1
-            right += 1
-
-        return s[start:(start + min_len)] if min_len != float('inf') else ""
+        ans = float('inf')
+        start = end = -1
+        while r < m:
+            if s[r] in ft:
+                fs[s[r]] += 1
+                if fs[s[r]] == ft[s[r]]:
+                    valid += 1
+            while valid == len(ft):
+                if r - l + 1 < ans:
+                    start = l
+                    end = r
+                    ans = r - l + 1
+                if s[l] in ft:
+                    fs[s[l]] -= 1
+                    if fs[s[l]] < ft[s[l]]:
+                        valid -= 1
+                l += 1
+            r += 1
+        return "" if ans == float('inf') else s[start:end+1]
+        
