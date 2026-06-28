ec80dbd7de9adcfcbb018ba03d6621515e84b168
People <chen.zhaom@northeastern.edu>
2026-06-28 23:36:45 +0800

    Time: 185 ms (41.46%), Space: 42.3 MB (25.37%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(speed)
+        time = [0] * n
+        for i, (pos, sp) in enumerate(sorted(zip(position, speed))):
+            time[i] = (target - pos) / sp
+        st = []
+        for t in time:
+            while st and st[-1] <= t:
+                st.pop()
+            st.append(t)
+
+        return len(st)
