dcfb7753b393d7132fb0a8ed9181d4850abb835a
People <chen.zhaom@northeastern.edu>
2026-06-29 15:35:15 +0800

    Time: 3 ms (46.86%), Space: 19.6 MB (41.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -7,7 +7,7 @@ class Solution:
         # 这个边界非常重要，L可以落在整个数组的有边界外，表示右侧段个数为零
         l, r = 0, m
         while l <= r:
-            div1 = l + (r - l) // 2
+            div1 = (l + r) // 2
             div2 = (m + n + 1) // 2 - div1
             
             div1_left = -float('inf') if div1 == 0 else nums1[div1 - 1]
@@ -15,7 +15,7 @@ class Solution:
             div2_left = -float('inf') if div2 == 0 else nums2[div2 - 1]
             div2_right = float('inf') if div2 == n else nums2[div2]
 
-            if div1_left <= div2_right and div1_right >= div2_left:
+            if div1_left <= div2_right and div2_left <= div1_right:
                 if (m + n) % 2 == 1:
                     return max(div1_left, div2_left)
                 else:
