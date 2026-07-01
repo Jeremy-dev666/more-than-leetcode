71cc209fa148b9fd189f51f6d787551a879c63b3
People <chen.zhaom@northeastern.edu>
2026-07-01 23:42:03 +0800

    Time: 0 ms (100%), Space: 19.2 MB (98.8%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,19 +5,18 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        p0 = dummy = ListNode(next=head)
+        ptr = dummy = ListNode(next=head)
         for _ in range(left - 1):
-            p0 = p0.next
-
-        pre = None
-        cur = p0.next
+            ptr = ptr.next
+        
+        prev = None
+        cur = ptr.next
         for _ in range(right - left + 1):
             nxt = cur.next
-            cur.next = pre
-            pre = cur
+            cur.next = prev
+            prev = cur
             cur = nxt
-
-        # 见视频
-        p0.next.next = cur
-        p0.next = pre
+        
+        ptr.next.next = cur
+        ptr.next = prev
         return dummy.next
