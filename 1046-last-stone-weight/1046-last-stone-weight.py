d61db8e52770395c9855552aa0b4f32db47f59db
People <chen.zhaom@northeastern.edu>
2026-07-07 17:53:08 +0800

    Time: 1 ms (21.48%), Space: 19.4 MB (69.11%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -6,11 +6,7 @@ class Solution:
         while len(q) > 1:
             x = heapq.heappop_max(q)
             y = heapq.heappop_max(q)
-            if x < y:
-                y = y - x
-                heapq.heappush_max(q, y)
-            elif x > y:
-                x = x - y
-                heapq.heappush_max(q, x)
+            if x > y:
+                heapq.heappush_max(q, x - y)
         return q[0] if q else 0
         
