fcfaa020ffaafa22af4e1843cbcb7ef729d325bd
People <chen.zhaom@northeastern.edu>
2026-07-08 22:58:23 +0800

    Time: 3 ms (95.49%), Space: 19.6 MB (63.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,17 +2,18 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         ans = []
-        def backtrack(path, start):
-            nonlocal ans
-            if sum(path) == target:
+        def backtrack(path, start, remain):
+            if remain == 0:
                 ans.append(list(path))
-            elif sum(path) > target:
                 return
 
             for i in range(start, len(candidates)):
+                # 剪枝
+                if candidates[i] > remain:
+                    break
                 path.append(candidates[i])
-                backtrack(path, i)
+                backtrack(path, i, remain - candidates[i])
                 path.pop()
 
-        backtrack([], 0)
+        backtrack([], 0, target)
         return ans
