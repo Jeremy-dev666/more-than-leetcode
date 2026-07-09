56d1215e515138759e09732a9fab2f7c2f8b5602
People <chen.zhaom@northeastern.edu>
2026-07-09 21:51:21 +0800

    Time: 0 ms (100%), Space: 19.3 MB (39.6%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,22 @@
+MAPPING = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dic = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
-        }
-        def backtrack(path, index):
-            if len(path) == len(digits):
-                ans.append("".join(path))
+        ans = []
+        n = len(digits)
+
+        def dfs(path, start):
+            if len(path) == n:
+                ans.append(''.join(path))
                 return
-            
-            number = digits[index]
-            letters = dic[number]
-            for l in letters:
-                path.append(l)
-                backtrack(path, index + 1)
+
+            number = digits[start]
+            letters = MAPPING[int(number)]
+            for c in letters:
+                path.append(c)
+                dfs(path, start+1)
                 path.pop()
 
-        ans = []
-        backtrack([], 0)
+        dfs([], 0)
         return ans
+            
