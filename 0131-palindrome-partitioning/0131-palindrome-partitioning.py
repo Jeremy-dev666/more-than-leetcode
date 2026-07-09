8c720a9c2218fdb2e2adea3105ef3f1dc031d83c
People <chen.zhaom@northeastern.edu>
2026-07-09 21:44:09 +0800

    Time: 42 ms (66.25%), Space: 33.6 MB (65.91%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,24 +2,20 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ans = []
         n = len(s)
-        
+
         # 子串s[start:i + 1]
-        def dfs(path, start, i):
-            if i == n:
+        def dfs(path, start):
+            if start == n:
                 ans.append(path.copy())
                 return
             
-            # 不分割，继续递归
-            if i < n - 1:
-                dfs(path, start, i + 1)
+            # 枚举分割的位置
+            for i in range(start, n):
+                t = s[start:i+1]
+                if t == t[::-1]:
+                    path.append(t)
+                    dfs(path, i+1)
+                    path.pop()
 
-            # 分割
-            # 验证是否是回文
-            t = s[start:i+1]
-            if t == t[::-1]:
-                path.append(t)
-                dfs(path, i+1, i+1)
-                path.pop()
-
-        dfs([], 0, 0)
+        dfs([], 0)
         return ans
