aed4081a0daa326419750556989a8178b67fd85e
People <chen.zhaom@northeastern.edu>
2026-07-09 21:34:59 +0800

    Time: 41 ms (69.54%), Space: 33.5 MB (73.6%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,25 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        self.ans = []
+        ans = []
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                if s[i] == s[j] and (j - i < 2 or dp[i + 1][j - 1]):
-                    dp[i][j] = True
-
-        self.backtrack(s, dp, 0, [])
-        return self.ans
-
-    def backtrack(self, s, dp, left, path) -> None:
-        n = len(s)
-        if left == n:
-            self.ans.append(list(path))
-            return
         
-        for right in range(left, n):
-            if not dp[left][right]:
-                continue
-            path.append(s[left:right+1])
-            self.backtrack(s, dp, right + 1, path)
-            path.pop()
+        # 子串s[start:i + 1]
+        def dfs(path, start, i):
+            if i == n:
+                ans.append(path.copy())
+                return
+            
+            # 不分割，继续递归
+            if i < n - 1:
+                dfs(path, start, i + 1)
+
+            # 分割
+            # 验证是否是回文
+            t = s[start:i+1]
+            if t == t[::-1]:
+                path.append(t)
+                dfs(path, i+1, i+1)
+                path.pop()
+
+        dfs([], 0, 0)
+        return ans
