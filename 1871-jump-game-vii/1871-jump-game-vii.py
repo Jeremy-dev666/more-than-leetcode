187f45f776cc9a65412264e983f8f1ab8170b1a4
People <chen.zhaom@northeastern.edu>
2026-07-25 20:21:53 +0800

    Time: 106 ms (95.08%), Space: 20.7 MB (70.01%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.py
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -1,19 +1,19 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
-        if s[-1] == "1":
-            return False
         n = len(s)
-        reach = [False] * n
-        reach[0] = True
+        can_reaches = [False] * n
+        can_reaches[0] = True
+
         j = 1
-        for i, c in enumerate(s):
-            if c == "0" and reach[i]:
-                farthest = min(i + maxJump, n - 1)
+        for i, ch in enumerate(s):
+            if ch == '0' and can_reaches[i]:
+                # 注意 j 只会增大，不会减小，所以总体时间复杂度是 O(n)
+                mx = min(i + maxJump, n - 1)
                 j = max(j, i + minJump)
-                while j <= farthest:
-                    reach[j] = True
+                while j <= mx:                    
+                    can_reaches[j] = True  # 可以跳到 j
                     j += 1
                 if j == n:
                     break
 
-        return reach[-1]
+        return s[-1] == '0' and can_reaches[-1]
