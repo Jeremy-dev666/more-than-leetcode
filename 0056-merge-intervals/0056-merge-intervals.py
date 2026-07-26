438533d644a02bce8c26f102e84279fa7dc1fb09
People <chen.zhaom@northeastern.edu>
2026-07-26 18:45:25 +0800

    Time: 0 ms (100%), Space: 22.7 MB (45.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,15 +1,23 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        # 将区间按照首元素升序排序
         intervals.sort(key=lambda x:x[0])
-        ans = []
+
+        # 取第一组区间初始化，从第二组区间开始遍历
+        n = len(intervals)
         prev = intervals[0]
+        ans = []
 
         for cur in intervals[1:]:
+            # 验证当前区间能否与上一区间合并
+            # 若能则继续合并，不能则将上一区间加入结果集
             if cur[0] <= prev[1]:
-                prev[1] = max(cur[1], prev[1])
+                prev[1] = max(prev[1], cur[1])
             else:
                 ans.append(prev)
                 prev = cur
-
+        
         ans.append(prev)
         return ans
+                
