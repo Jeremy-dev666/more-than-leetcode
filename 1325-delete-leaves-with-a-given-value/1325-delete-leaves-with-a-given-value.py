bde58dbe9db8d78b569168052e2e4bcb740b0c8c
People <chen.zhaom@northeastern.edu>
2026-08-05 16:28:18 +0800

    Time: 0 ms (100%), Space: 19.8 MB (75.37%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
@@ -6,14 +6,13 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        
         if not root:
             return None
-        
+
         root.left = self.removeLeafNodes(root.left, target)
         root.right = self.removeLeafNodes(root.right, target)
-        # 递归遍历，如果左右子节点为空（说明是叶子节点）
-        # 且当前节点值==目标值那么可以删除，返回None
+        
         if not root.left and not root.right and root.val == target:
             return None
-        # 否则返回当前节点
         return root
