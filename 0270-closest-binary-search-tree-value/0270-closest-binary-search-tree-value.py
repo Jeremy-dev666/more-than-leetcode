72309a3b1fbdb647e015cd3d88701ca25ee076fa
People <chen.zhaom@northeastern.edu>
2026-08-13 09:51:06 +0800

    Time: 0 ms (100%), Space: 20.8 MB (95.89%) - LeetHub

---
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        closest = root.val
+        node = root
+        while node:
+            if abs(node.val - target) < abs(closest - target):
+                closest = node.val
+            elif abs(node.val - target) == abs(closest - target):
+                closest = min(closest, node.val)
+            node = node.left if target < node.val else node.right
+        return closest
