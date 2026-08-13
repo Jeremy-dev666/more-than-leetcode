61cadb767da3dbf5c9a7bf6fdd6da56469af9968
People <chen.zhaom@northeastern.edu>
2026-08-13 17:54:47 +0800

    Time: 127 ms (76.59%), Space: 26.8 MB (89.6%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -1,20 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
-        nodes = {}  # val -> TreeNode
+        nodes = {}
         children = set()
 
-        # 建树
-        for p, c, is_left in descriptions:
-            if p not in nodes:
-                nodes[p] = TreeNode(p)
-            if c not in nodes:
-                nodes[c] = TreeNode(c)
-            if is_left:
-                nodes[p].left = nodes[c]
-            else:
-                nodes[p].right = nodes[c]
-            children.add(c)  # c 不是根节点
+        for pa, ch, is_left in descriptions:
+            if pa not in nodes:
+                nodes[pa] = TreeNode(pa)
+            if ch not in nodes:
+                nodes[ch] = TreeNode(ch)
 
-        for p, node in nodes.items():
-            if p not in children:  # node 是根节点
+            if is_left:
+                nodes[pa].left = nodes[ch]
+            else:
+                nodes[pa].right = nodes[ch]
+
+            children.add(ch)
+
+        for val, node in nodes.items():
+            if val not in children:
                 return node
