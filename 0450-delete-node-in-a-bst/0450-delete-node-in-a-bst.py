3f5ec96ba3c6022a4831c37b24c08ee85a322a41
People <chen.zhaom@northeastern.edu>
2026-08-14 10:45:53 +0800

    Time: 0 ms (100%), Space: 22 MB (42.94%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -7,24 +7,31 @@
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if root is None:
-            return root
-        # find in left subtree
+            return None
+
+        # 找要删除的节点
+        # 如果比当前节点小，那么没找到，往左子树递归继续找
         if key < root.val:
             root.left = self.deleteNode(root.left, key)
-        # find in right subtree
+        # 如果比当前节点大，那么没找到，往右子树递归继续找
         elif key > root.val:
             root.right = self.deleteNode(root.right, key)
-        # when val == key, find the largest val in left subtree or smallest val in right subtree
+        # 当root.val = key时，分情况讨论如何删除
         else:
+            # 只有单子节点时直接顶替上来
             if not root.left:
                 return root.right
             if not root.right:
                 return root.left
-
-            ptr = root.left
-            while ptr.right:
-                ptr = ptr.right
+            
+            # 双子树（这里选择找右子树最小值来顶替）
+            ptr = root.right
+            while ptr.left:  # 往左走到底找到最小值
+                ptr = ptr.left
+            # 把值替换掉，并且同样地递归把这个最小值节点删除
             root.val = ptr.val
-            root.left = self.deleteNode(root.left, ptr.val)
+            root.right = self.deleteNode(root.right, ptr.val)
 
         return root
+            
+
