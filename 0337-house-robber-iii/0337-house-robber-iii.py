8b9b3fa6a96addf34970e1602c17a33282a76f10
People <chen.zhaom@northeastern.edu>
2026-08-14 14:48:52 +0800

    Time: 4 ms (40.35%), Space: 20.6 MB (99.04%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,18 +6,18 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        def dfs(node: Optional[TreeNode]) -> Tuple[int, int]:
-            # 递归存一堆元组信息（抢或不抢的价值）
+        
+        def dfs(node):
             if node is None:
                 return 0, 0
-            l_rob, l_not_rob = dfs(node.left)
-            r_rob, r_not_rob = dfs(node.right)
 
-            # 子树都不抢，那么父节点价值可以算上
-            par_rob = node.val + l_not_rob + r_not_rob
-            # 子树有一个抢，那么父节点就不能抢
-            par_not = max(l_rob, l_not_rob) + max(r_rob, r_not_rob)
+            l_rob, l_not = dfs(node.left)
+            r_rob, r_not = dfs(node.right)
 
-            return par_rob, par_not
+            prnt_rob = node.val + l_not + r_not
+            prnt_not = max(l_rob, l_not) + max(r_rob, r_not)
+
+            return prnt_rob, prnt_not
 
         return max(dfs(root))
+            
